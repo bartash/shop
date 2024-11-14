777b5c379433585fe4df74e7dc3ff440a1e63b48
Andrew Sherman <andrew@ohair-sherman.com>
2024-11-13 17:50:29 -0800

    deal with varied data

--- shop.py
+++ shop.py
@@ -14,7 +14,7 @@ class WondrousItem:
   price: int
   half_price: int
   upgrade: str
-  cl: int
+  caster_level: int
   weight: float
   strength: str
   school: str
@@ -50,21 +50,36 @@ def read_wondrous_items(filename):
       price = locale.atoi(row[4]) if row[4] else None
       half_price = locale.atoi(row[5]) if row[5] else None
       upgrade = row[6]
-      cl = int(row[7])
-      weight = float(row[8])
+      raw_caster_level = row[7]
+      if raw_caster_level == "Varied":
+        raw_caster_level = "20"
+      if raw_caster_level == "?":
+        raw_caster_level = "20"
+      print(f'caster_level={raw_caster_level}')
+      caster_level = int(raw_caster_level) if raw_caster_level else None
+      weight_raw = row[8]
+      if weight_raw == '?':
+        weight_raw = "0"
+      if weight_raw == '':
+        weight_raw = "0"
+      # print(f'weight={weight_raw}')
+      weight = float(weight_raw)
       strength = row[9]
       school = row[10]
       crafting_feat = row[11]
       spells_needed = row[12]
       other_reqs = row[13]
-      base_crafting_dc = int(row[14])
-      num_req = int(row[15])
+      raw_base_crafting_dc = row[14]
+      print(f'base_crafting_dc={raw_base_crafting_dc}')
+      base_crafting_dc = int(raw_base_crafting_dc)
+      # num_req = int(row[15])
+      num_req = 0 # not sure what this is
       source = row[16]
       url = row[17]
       notes = row[18]
 
       # Create a WondrousItem object and append it to the list
-      item = WondrousItem(name, slot, description, view_desc, price, half_price, upgrade, cl, weight, strength, school, crafting_feat, spells_needed, other_reqs, base_crafting_dc, num_req, source, url, notes)
+      item = WondrousItem(name, slot, description, view_desc, price, half_price, upgrade, caster_level, weight, strength, school, crafting_feat, spells_needed, other_reqs, base_crafting_dc, num_req, source, url, notes)
       items.append(item)
   return items
 
